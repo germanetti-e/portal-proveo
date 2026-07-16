ef24d5b202efa2b0c23be89b6a29c7a3d6c95c5f
germanetti-e <germanettie@gmail.com>
2026-07-16 14:42:12 -0500

    Update catalogo.js

--- js/catalogo.js
+++ js/catalogo.js
@@ -8,3 +8,43 @@
 ========================== */
 
 const API_URL = "https://script.google.com/macros/s/AKfycbxEoH-PFVJTjR0tdug3EedfioGxxAm1a-Ed1SU4na5qNiuLe_QFl1qaOL_an-C7eXF8bg/exec";
+/* =======================================================
+   PROVEO
+   Catálogo dinámico
+======================================================= */
+
+/* ==========================
+   CONFIGURACIÓN
+========================== */
+
+const API_URL = "PEGA_AQUI_TU_URL_DEL_APPS_SCRIPT";
+
+/* ==========================
+   LEER CATÁLOGO
+========================== */
+
+async function cargarCatalogo() {
+
+    try {
+
+        const respuesta = await fetch(API_URL);
+
+        const productos = await respuesta.json();
+
+        console.log(productos);
+
+    }
+
+    catch(error){
+
+        console.error("Error cargando catálogo:", error);
+
+    }
+
+}
+
+/* ==========================
+   INICIAR
+========================== */
+
+cargarCatalogo();
