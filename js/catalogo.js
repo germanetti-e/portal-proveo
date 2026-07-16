dab305ae9cd1ba093bb71ad9c3ebfd859c37bdc5
germanetti-e <germanettie@gmail.com>
2026-07-16 14:41:14 -0500

    Sprint 3 - JS - Parte 1

---
+++ js/catalogo.js
@@ -0,0 +1,10 @@
+/* =======================================================
+   PROVEO
+   Catálogo dinámico
+======================================================= */
+
+/* ==========================
+   CONFIGURACIÓN
+========================== */
+
+const API_URL = "https://script.google.com/macros/s/AKfycbxEoH-PFVJTjR0tdug3EedfioGxxAm1a-Ed1SU4na5qNiuLe_QFl1qaOL_an-C7eXF8bg/exec";
