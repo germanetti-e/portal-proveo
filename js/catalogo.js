a78ab06fca2cbd6da2b06c7c16945c7fef879546
germanetti-e <germanettie@gmail.com>
2026-07-16 14:50:10 -0500

    Update catalogo.js

--- js/catalogo.js
+++ js/catalogo.js
@@ -8,16 +8,6 @@
 ========================== */
 
 const API_URL = "https://script.google.com/macros/s/AKfycbxEoH-PFVJTjR0tdug3EedfioGxxAm1a-Ed1SU4na5qNiuLe_QFl1qaOL_an-C7eXF8bg/exec";
-/* =======================================================
-   PROVEO
-   Catálogo dinámico
-======================================================= */
-
-/* ==========================
-   CONFIGURACIÓN
-========================== */
-
-const API_URL = "PEGA_AQUI_TU_URL_DEL_APPS_SCRIPT";
 
 /* ==========================
    LEER CATÁLOGO
