8de026fe4468d56e564c230e98da745a34cae3d5
germanetti-e <germanettie@gmail.com>
2026-07-22 12:26:00 -0500

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -5,13 +5,6 @@
 
 
 /* ==========================
-   CONFIGURACIÓN
-========================== */
-
-const API_URL = "https://script.google.com/macros/s/AKfycbxEoH-PFVJTjR0tdug3EedfioGxxAm1a-Ed1SU4na5qNiuLe_QFl1qaOL_an-C7eXF8bg/exec";
-
-
-/* ==========================
    VARIABLES
 ========================== */
 
