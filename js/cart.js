9dbd8610379dd2e2dcee044ad4727e0326be9a34
germanetti-e <germanettie@gmail.com>
2026-07-29 11:13:47 -0500

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -569,3 +569,25 @@ function actualizarEstadoAbastecimiento(productos){
     }
 
 }
+
+/* ==========================
+   CONTADOR DEL CARRITO
+========================== */
+
+function actualizarContadorCarrito(){
+
+    const contador = document.getElementById("cart-counter");
+
+    if(!contador) return;
+
+    const carrito = cargarCarrito();
+
+    const total = carrito.reduce((suma, producto) => {
+
+        return suma + Number(producto.cantidad);
+
+    }, 0);
+
+    contador.textContent = total;
+
+}
